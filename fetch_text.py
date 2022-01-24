68f0007fd4074093338575a4a97a6494bc6de0da
Nimish Bajaj <nimishbajaj@MacBook-Air-10.local>
2022-01-24 01:36:41 -0500

    initial commit

---
+++ fetch_text.py
@@ -0,0 +1,25 @@
+import wikipedia
+import time
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
+
+titles = pd.read_csv("/Users/nimishbajaj/PycharmProjects/scrapeWiki/political_topics_l1.csv")
+print(titles.head())
+# titles = titles.head(2)
+
+def get_wiki_text(title: str):
+    try:
+        wiki = wikipedia.page(title)
+        text = wiki.content
+        return " ".join(text.split())
+    except:
+        return "NA"
+
+titles["text"] = titles.progress_apply(lambda row: get_wiki_text(row.title), axis=1)
+
+print(titles.head())
+
+titles.to_csv(f"/Users/nimishbajaj/PycharmProjects/scrapeWiki/titles_with_text_{time.time()}.csv", index=False)
+
+
